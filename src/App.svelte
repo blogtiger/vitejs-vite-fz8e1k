267858465d61bb607fe8f9349ac38bbe56761dc7
Tim Cui <blogtiger@gmail.com>
2024-09-30 06:29:50 -0700

    working with multi tables and connections

--- src/App.svelte
+++ src/App.svelte
@@ -15,42 +15,134 @@
   import Films from './lib/tables/Films.svelte';
   import { each } from 'svelte/internal';
 
-  
-  let config = [
-    {
-      title: "abc",
-      id: "abc",
-      position: { x: 0, y: -500 },
-      data: [
-        { key: 'id1', type: 'type1', note: 'note1' },
-        { key: 'id2', type: 'type2', note: 'note2' },
-        { key: 'id3', type: 'type3', note: 'note3' }
-      ],
-      anchors: [
-        { type: 'input', key: 'id1', connections: [['table_id', 'anchor_id']], direction: "west" },
-        { type: 'input', key: 'id2', connections: [['ass', 'ss']], direction: "east" },
-        { type: 'output', key: 'id3', id: "output_anchor1", direction: "west" }
-      ]
-    }
-  ];
+  let config=[
+  {
+    "title": "Films",
+    "id": "films",
+    "position": { "x": 1090, "y": 150 },
+    "anchors": [
+      { "type": "output", "key": "id", "id": "films_anchor1", "direction": "west" }
+    ],
+    "data": [
+      { "key": "id", "type": "bigint", "note": "Primary Key, autoincrement()" },
+      { "key": "title", "type": "varchar", "note": "not null" },
+      { "key": "episode_id", "type": "varchar", "note": "not null" },
+      { "key": "opening_crawl", "type": "varchar", "note": "not null" },
+      { "key": "director", "type": "varchar", "note": "not null" },
+      { "key": "producer", "type": "varchar", "note": "not null" },
+      { "key": "release_date", "type": "date", "note": "not null" }
+    ]
+  },
+  {
+    "title": "People",
+    "id": "people",
+    "position": { "x": -300, "y": -300 },
+    "anchors": [
+      { "type": "output", "key": "id", "id": "people_anchor1", "direction": "west" },
+      { "type": "input", "key": "species_id", "id": "people_anchor3", "edge": "{pkey}", "connections": [["species", "species_anchor1"]], "direction": "east" },
+      { "type": "input", "key": "homeworld_id", "id": "people_anchor4", "edge": "{pkey}", "connections": [["planets", "planets_anchor1"]], "direction": "east" }
+    ],
+    "data": [
+      { "key": "id", "type": "bigint", "note": "Primary Key, autoincrement()" },
+      { "key": "name", "type": "varchar", "note": "not null" },
+      { "key": "mass", "type": "varchar", "note": "optional" },
+      { "key": "hair_color", "type": "varchar", "note": "optional" },
+      { "key": "skin_color", "type": "varchar", "note": "optional" },
+      { "key": "eye_color", "type": "varchar", "note": "optional" },
+      { "key": "birth_year", "type": "varchar", "note": "optional" },
+      { "key": "gender", "type": "varchar", "note": "optional" },
+      { "key": "species_id", "type": "bigint", "note": "Foreign Key to Species" },
+      { "key": "homeworld_id", "type": "bigint", "note": "Foreign Key to Planets" },
+      { "key": "height", "type": "int", "note": "optional" }
+    ]
+  },
+  {
+    "title": "Pilots",
+    "id": "pilot",
+    "position": { "x": -300, "y": 300 },
+    "anchors": [
+      { "type": "input", "key": "person_id", "id": "pilots_anchor1", "connections": [["people", "people_anchor1"]], "direction": "west" }
+    ],
+    "data": [
+      { "key": "id", "type": "bigint", "note": "Primary Key, autoincrement()" },
+      { "key": "person_id", "type": "bigint", "note": "Foreign Key to People, not null" },
+      { "key": "vessel_id", "type": "bigint", "note": "not null" }
+    ]
+  },
+  {
+    "title": "Planets in Films",
+    "id": "planets_in_films",
+    "position": { "x": 700, "y": 400 },
+    "anchors": [
+      { "type": "input", "key": "id", "connections": [["planets", "planets_anchor2"]], "direction": "west" },
+      { "type": "input", "key": "film_id", "connections": [["films", "films_anchor1"]], "direction": "east" }
+    ],
+    "data": [
+      { "key": "id", "type": "bigint", "note": "Primary Key, autoincrement()" },
+      { "key": "film_id", "type": "bigint", "note": "Foreign Key to Films, not null" },
+      { "key": "planet_id", "type": "bigint", "note": "Foreign Key to Planets, not null" }
+    ]
+  },
+  {
+    "title": "Planets",
+    "id": "planets",
+    "position": { "x": 240, "y": 600 },
+    "anchors": [
+      { "type": "output", "key": "id", "id": "planets_anchor1", "direction": "west" },
+      { "type": "output", "key": "id", "id": "planets_anchor2", "direction": "east" }
+    ],
+    "data": [
+      { "key": "id", "type": "bigint", "note": "Primary Key, autoincrement()" },
+      { "key": "name", "type": "varchar", "note": "optional" },
+      { "key": "mass", "type": "varchar", "note": "optional" }
+    ]
+  },
+  {
+    "title": "Species in Films",
+    "id": "species_in_films",
+    "position": { "x": 700, "y": 80 },
+    "anchors": [
+      { "type": "input", "key": "id", "connections": [["species", "species_anchor2"]], "direction": "west" },
+      { "type": "input", "key": "film_id", "connections": [["films", "films_anchor1"]], "direction": "east" }
+    ],
+    "data": [
+      { "key": "id", "type": "bigint", "note": "Primary Key, autoincrement()" },
+      { "key": "film_id", "type": "bigint", "note": "Foreign Key to Films, not null" },
+      { "key": "species_id", "type": "bigint", "note": "Foreign Key to Species, not null" }
+    ]
+  },
+  {
+    "title": "Species",
+    "id": "species",
+    "position": { "x": 225, "y": -280 },
+    "anchors": [
+      { "type": "output", "key": "id", "id": "species_anchor1", "direction": "west" },
+      { "type": "output", "key": "id", "id": "species_anchor2", "direction": "east" },
+      { "type": "input", "key": "homeworld_id", "id": "species_anchor3", "connections": [["planets", "planets_anchor1"]], "direction": "west" }
+    ],
+    "data": [
+      { "key": "id", "type": "bigint", "note": "Primary Key, autoincrement()" },
+      { "key": "name", "type": "varchar", "note": "not null" },
+      { "key": "classification", "type": "varchar", "note": "optional" },
+      { "key": "average_height", "type": "varchar", "note": "optional" },
+      { "key": "average_lifespan", "type": "varchar", "note": "optional" },
+      { "key": "hair_color", "type": "varchar", "note": "optional" },
+      { "key": "skin_color", "type": "varchar", "note": "optional" },
+      { "key": "eye_colors", "type": "varchar", "note": "optional" },
+      { "key": "language", "type": "varchar", "note": "optional" },
+      { "key": "homeworld_id", "type": "bigint", "note": "Foreign Key to Planets, optional" }
+    ]
+  }
+]
+
 
 </script>
 <body>
   <Svelvet fitView controls minimap>
-    <Vessel/>
-    <People/>
-    <Pilot/>
-    <Planet/>
-    <Species/>
-    <Starship/>
-    <PeopleFilms/>
-    <SpeciesInFilms/>
     {#each config as tableCofnig}
     <PlanetsInFilms config={tableCofnig}/>
     {/each}
-    <VesselsInFilms/>
-    <Films/>
-    <ThemeToggle main='dark' alt='light' slot='toggle'/>
+    <ThemeToggle main='light' alt='dark' slot='toggle'/>
   </Svelvet>
 </body>
 
